23b66572c3d1e8484458d07982a5df3f11e8798f
przemekk922 <przemyslaw.kalinowski@wakacje.pl>
2022-12-09 17:34:45 +0100

    Add account profile

--- App.js
+++ App.js
@@ -8,13 +8,15 @@ const paperTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#FFD024',
-    background: '#1E1E1E',
-    error: '#DE0000',
-    white: '#fff',
-    gray: '#AAAAAA',
-    available: '#6AB880',
-    unavailable: '#ED7566'
+    primary: "#FFD024",
+    background: "#1E1E1E",
+    error: "#DE0000",
+    white: "#fff",
+    gray: "#AAAAAA",
+    darkGray: "#323232",
+    backgroundGray: "#D9D9D9",
+    available: "#6AB880",
+    unavailable: "#ED7566",
   },
 }
 export default function App() {
@@ -31,7 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: paperTheme.colors.background,
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: paperTheme.colors.gray,
   },
 });
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,13 +1,122 @@
 import React from 'react'
-import { View,Text } from 'react-native'
+import {View, Text,ScrollView, StyleSheet} from 'react-native'
+import { useTheme, Avatar, Button, Divider } from "react-native-paper";
+import UserPhotoPlug from "../components/SvgIcons/UserPhotoPlug";
+import PlusIcon from "../components/SvgIcons/PlusIcon";
 
 const Profile = () => {
+  const theme = useTheme();
+
+  const styles = StyleSheet.create({
+    userAccount: {
+      flexDirection: 'row',
+      marginTop: 16
+    },
+    avatar: {
+      backgroundColor: theme.colors.background
+    },
+    userData: {
+      marginLeft: 24
+    },
+    title: {
+      marginBottom: 8,
+      fontSize: 16,
+      fontWeight: '600',
+      color: theme.colors.gray
+    },
+    info: {
+      marginBottom: 4,
+      color: theme.colors.gray,
+      lineHeight: 20
+    },
+    divider: {
+      marginTop: 24,
+      marginBottom: 24
+    },
+    tableWrapper: {
+      width: '100%',
+      height: '100%',
+      borderWidth: 1,
+      borderColor: theme.colors.gray,
+      borderRadius: 4
+    },
+    tableTop: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 16
+    },
+    button: {
+      borderRadius: 5
+    },
+    topBar: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 16,
+      borderBottomColor: theme.colors.gray,
+      borderBottomWidth: 1
+    },
+    rank: {
+    },
+    tableCell: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 12,
+      marginHorizontal: 16,
+      borderBottomColor: theme.colors.gray,
+      borderBottomWidth: 1
+    },
+    status: {
+      width: 16,
+      height: 16,
+      borderRadius: '50%',
+      backgroundColor: theme.colors.available,
+    },
+    playerName: {
+      color: theme.colors.gray,
+      lineHeight: 24
+    },
+    result: {
+      color: theme.colors.gray
+    }
+  });
+
   return (
-    <View>
-      <Text>
-      Profile
-      </Text>
+    <View style={{ backgroundColor: theme.colors.background, height: '100%', padding: 24 }}>
+      <View style={styles.userAccount}>
+        <Avatar.Image size={98} source={UserPhotoPlug} style={styles.avatar}/>
+        <View style={styles.userData}>
+          <Text style={styles.title}>Mateusz Bukowski</Text>
+          <Text style={styles.info}>Ranga: Nowicjusz</Text>
+          <Text style={styles.info}>Wygrane: 6</Text>
+          <Text style={styles.info}>Przegrane: 2</Text>
+        </View>
       </View>
+      <Divider style={styles.divider}/>
+      <View style={styles.tableWrapper}>
+        <View style={styles.tableTop}>
+          <Text style={styles.title}>Rozegrane mecze</Text>
+          <View style={styles.button}>
+            <Button buttonColor={theme.colors.darkGray} style={styles.button} icon={PlusIcon}>
+              <Text style={{color: theme.colors.white}}>Dodaj wynik</Text>
+            </Button>
+          </View>
+        </View>
+        <View style={styles.topBar}>
+          <Text style={styles.info}>Status</Text>
+          <Text style={styles.info}>Przeciwnik</Text>
+          <Text style={styles.info}>Wynik</Text>
+        </View>
+        <ScrollView style={styles.rank}>
+          <View style={styles.tableCell}>
+            <View style={styles.status}></View>
+            <Text style={styles.playerName}>Przemysław Kalinowski</Text>
+            <Text style={styles.result}>3:0</Text>
+          </View>
+        </ScrollView>
+      </View>
+    </View>
   )
 }
 
