0b28803684426dc584e1b4302c751f56d9a5621e
Kamil Zieliński <44470634+kamilzielinski97@users.noreply.github.com>
2022-12-10 01:44:24 +0100

    kamil-home-modals add modal with action (#20)
    Co-authored-by: kazielinski <Kamil.Zielinski@wakacje.pl>

---
+++ components/CardComponentAddScore.jsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { StyleSheet, View} from 'react-native'
+import { Button,Text  } from 'react-native-paper';
+
+const CardComponentAddScore = ({icon, firstText, secondText, description, left, handleSetVisibleModal, handleSetInvitationModal}) => {
+
+  const styles = StyleSheet.create({
+	container: {
+	  display: 'flex',
+	  flexDirection: 'row',
+	  alignItems: 'center',
+	  position: 'right',
+	  marginTop: 8
+	},
+	button: {
+	  height: 120,
+	  width: 120,
+	  borderRadius: 10,
+	  backgroundColor:'#323232',
+	  display: 'flex',
+	  flexDirection: 'column',
+	  justifyContent: 'center',
+	  rotate: "-3deg"
+	},
+	buttonRight: {
+	  height: 120,
+	  width: 120,
+	  borderRadius: 10,
+	  backgroundColor:'#323232',
+	  display: 'flex',
+	  justifyContent: 'center',
+	  rotate: "3deg"
+	},
+	paragraph: {
+	  display: 'flex',
+	  alignItems: 'center',
+	  flexDirection: 'column',
+	  color: '#FFFFFF',
+	},
+	paragraphFirst: {
+	  display: 'flex',
+	  alignItems: 'center',
+	  flexDirection: 'column',
+	  color: '#FFFFFF',
+	  marginTop: 4,
+	},
+	description: {
+	  fontSize: 14,
+	  width: 145,
+	  marginLeft: 8,
+	  lineHeight: 24,
+	  color: '#CACACA',
+	},
+  });
+
+return (
+  <View style={styles.container}>
+	{left && <Text variant="titleSmall" style={styles.description}>{description}</Text>}
+	<Button
+	  style={left ? styles.buttonRight :  styles.button}
+	  onPress={handleSetVisibleModal}
+	  contentStyle={{
+		flexDirection: 'column',
+		justifyContent: 'center',
+		height: '100%'
+	  }}
+   >
+	  <View style={{
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100%'
+	  }}>
+		{icon}
+	  <Text style={styles.paragraphFirst}>
+		{firstText}
+	  </Text>
+	  <Text style={styles.paragraph}>
+		{secondText}
+	  </Text>
+	  </View>
+	</Button>
+	{!left && <Text variant="titleSmall" style={styles.description}>{description}</Text>}
+  </View>
+  )
+}
+
+export default CardComponentAddScore;
--- screens/Home.js
+++ screens/Home.js
@@ -1,25 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View } from "react-native";
 import PingMeIcon from "../assets/Icons/PingMeIcon";
-import CardComponent from '../components/CardComponent'
+import CardComponentAddScore from '../components/CardComponentAddScore'
 import RandomUser from "../assets/Icons/RandomUser";
 import AddIcon from "../assets/Icons/AddIcon";
 import PingPong from "../assets/Icons/PingPong";
 import Screen from "../components/Screen";
 import DrawOpponentModal from '../components/Modals/DrawOpponentModal'
 import SendInvitationModal from '../components/Modals/SendInvitationModal'
+import AddScoreModal from "../components/Modals/AddScoreModal";
+import CardComponentSendInvitation from "../components/CardComponentSendInvitation";
+import CardComponentDrawOpponent from "../components/CardComponentDrawOpponent";
 
 const Home = () => {
+  const [isVisibleScoreModal, setIsVisibleScoreModal] = useState(0);
+  const [isVisibleInvitationModal, setIsVisibleInvitationModal] = useState(0);
+  const [isVisibleDrawOpponentModal, setIsVisibleDrawOpponentModal] = useState(0);
 
+  const handleSetVisibleModal = () => {
+	setIsVisibleScoreModal(1)
+  }
+  const handleCloseModal = () => {
+	setIsVisibleScoreModal(0)
+  }
+
+  const handleSetInvitationModal = () => {
+	setIsVisibleInvitationModal(1)
+  }
+  const handleCloseInvitationModal = () => {
+	setIsVisibleInvitationModal(0)
+  }
+
+  const handleSetDrawOpponentModal = () => {
+	setIsVisibleDrawOpponentModal(1)
+  }
+  const handleCloseDrawOpponentModal = () => {
+	setIsVisibleDrawOpponentModal(0)
+  }
   return (
 	<Screen>
 		<View style={{alignItems: 'center'}}>
 	  <PingMeIcon />
-	  <CardComponent icon={<RandomUser /> }firstText={'Wylosuj'} secondText={'przeciwnika'} description={'Losuj przeciwników aby poznawać nowe osoby.'}/>
-	  <CardComponent icon={<PingPong /> }firstText={'Wyślij'} secondText={'zaproszenie'} description={'Wysyłaj zaproszenia do ulubionych rywali.'} left/>
-	  <CardComponent icon={<AddIcon /> }firstText={'Dodaj wynik'} secondText={'meczu'} description={'Dodawaj wyniki meczu aby brać udział w #grywalizacji!'}/>
+	  <CardComponentDrawOpponent
+		icon={<RandomUser /> }
+		firstText={'Wylosuj'}
+		secondText={'przeciwnika'}
+		description={'Losuj przeciwników aby poznawać nowe osoby.'}
+		handleSetDrawOpponentModal={handleSetDrawOpponentModal}
+	  />
+	  <CardComponentSendInvitation
+		icon={<PingPong /> }
+		firstText={'Wyślij'}
+		secondText={'zaproszenie'}
+		description={'Wysyłaj zaproszenia do ulubionych rywali.'}
+		left
+		handleSetInvitationModal={handleSetInvitationModal}
+	  />
+	  <CardComponentAddScore
+		icon={<AddIcon /> }
+		firstText={'Dodaj wynik'}
+		secondText={'meczu'}
+		description={'Dodawaj wyniki meczu aby brać udział w #grywalizacji!'}
+		handleSetVisibleModal={handleSetVisibleModal}
+	  />
 		</View>
-	  {/* <DrawOpponentModal /> */}
+	  <AddScoreModal isVisible={isVisibleScoreModal} handleCloseModal={handleCloseModal}/>
+	  <SendInvitationModal isVisible={isVisibleInvitationModal} handleCloseInvitationModal={handleCloseInvitationModal}/>
+	  <DrawOpponentModal isVisible={isVisibleDrawOpponentModal} handleCloseDrawOpponentModal={handleCloseDrawOpponentModal} />
 	</Screen>
   )
 }
